7956e2e048b9fea0d89356d570673f025bfe3e2f
sourabh <pedanekarsourabh@gmail.com>
2025-06-03 10:11:46 +0530

    week 1

---
+++ Patterns/RightAngleTringle.java
@@ -0,0 +1,9 @@
+
+public class RightAngleTringle {
+    public static void main(String[] args) {
+        // for( int j = 0; j)
+        for (int i = 0; i < 10; i++) {
+            System.err.println("*");
+        }
+    }    
+}
---
+++ helloJava/Simple.java
@@ -0,0 +1,5 @@
+class Simple{
+    public static void main(String[] args) {
+        System.out.println("i will  be very rich one day, that i know very well");
+    }
+}
